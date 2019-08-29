f7f0d69e82ffa6c2b9671c54274224d541b90446
Martin Roman <martin@localhost.localdomain>
2019-08-29 18:33:40 -0500

    Funcional

--- routes/index.js
+++ routes/index.js
@@ -40,41 +40,23 @@ router.get('/', (req, res) => {
 // Revisar estado cada 5 segundos
 const intervalCollector = client.collectDefaultMetrics({prefix: 'node_', timeout: 5000, register});
 
-const counter = new client.Counter({
-    name: "node_my_counter",
-    help: "This is my counter"
- });
-
  const gauge = new client.Gauge({
-    name: "node_my_gauge",
-    help: "This is my gauge"
- });
-
- const histogram = new client.Histogram({
-    name: "node_my_histogram",
-    help: "This is my histogram",
-    buckets: [0.1, 5, 15, 50, 100, 500]
+    name: "temperature",
+    help: "Último dato de temperatura registrado"
  });
  
- const summary = new client.Summary({
-    name: "node_my_summary",
-    help: "This is my summary",
-    percentiles: [0.01, 0.05, 0.5, 0.9, 0.95, 0.99, 0.999]
- });
-
-register.registerMetric(counter);
 register.registerMetric(gauge);
-register.registerMetric(histogram);
-register.registerMetric(summary);
 
 const rand = (low, high) => Math.random() * (high - low) + low;
 
-setInterval(() => {
+gauge.set(25.39);
+
+/*setInterval(() => {
    counter.inc(rand(0, 1));
    gauge.set(rand(0, 15));
    histogram.observe(rand(0,10));
    summary.observe(rand(0, 10));
-}, 1000);
+}, 1000);*/
 
 router.get('/metrics', (req, res) => {
    res.set('Content-Type', register.contentType);
