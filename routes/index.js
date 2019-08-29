ba572747667ffa553b6cc208ca2872af2c01d0ff
Martin Roman <martin@localhost.localdomain>
2019-08-29 18:22:31 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ const express = require('express');
 const router = express.Router();
 var cmd = require('node-cmd');
 
+// Prometheus
+const client = require('prom-client');
+const register = new client.Registry();
+
 // Zoom API
 var request = require("request");
 
@@ -22,7 +26,7 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/metrics', (req, res) => {
+/*router.get('/metrics', (req, res) => {
     
     res.setHeader("Content-Type", "text/plain; version=0.0.4; charset=utf-8");
     res.send(`
@@ -31,7 +35,52 @@ router.get('/metrics', (req, res) => {
     temperature 0.003035248
     `);
 
+});*/
+
+// Revisar estado cada 5 segundos
+const intervalCollector = client.collectDefaultMetrics({prefix: 'node_', timeout: 5000, register});
+
+const counter = new client.Counter({
+    name: "node_my_counter",
+    help: "This is my counter"
+ });
+
+ const gauge = new client.Gauge({
+    name: "node_my_gauge",
+    help: "This is my gauge"
+ });
+
+ const histogram = new client.Histogram({
+    name: "node_my_histogram",
+    help: "This is my histogram",
+    buckets: [0.1, 5, 15, 50, 100, 500]
+ });
+ 
+ const summary = new client.Summary({
+    name: "node_my_summary",
+    help: "This is my summary",
+    percentiles: [0.01, 0.05, 0.5, 0.9, 0.95, 0.99, 0.999]
+ });
+
+register.registerMetric(counter);
+register.registerMetric(gauge);
+register.registerMetric(histogram);
+register.registerMetric(summary);
+
+const rand = (low, high) => Math.random() * (high - low) + low;
+
+setInterval(() => {
+   counter.inc(rand(0, 1));
+   gauge.set(rand(0, 15));
+   histogram.observe(rand(0,10));
+   summary.observe(rand(0, 10));
+}, 1000);
+
+router.get('/metrics', (req, res) => {
+   res.set('Content-Type', register.contentType);
+   res.end(register.metrics());
 });
+
 
 // Exportar modulo
 module.exports = router;
